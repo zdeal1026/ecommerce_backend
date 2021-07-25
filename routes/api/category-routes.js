199a0e62b4f37b3902f6485c687752ca45e497cf
Zdeal <jedimastershake@gmail.com>
2021-07-24 23:12:04 -0400

    working category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  try {
+    const catgor = await Category.findAll({ 
+      incldue: [{ model: Product }],
+    });
+    res.status(200).json(catgor);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
-router.get('/:id', (req, res) => {
+  });
+
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  try {
+    const catgor = await Category.findById(req.params.id, {
+      included: [{ model: Product }],
+    });
+    if (!catgor) {
+      res.status(404).json({ message: "Id has no category"});
+      return;
+    }
+    res.status(200).json(catgor);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  });
 
 router.post('/', (req, res) => {
   // create a new category
