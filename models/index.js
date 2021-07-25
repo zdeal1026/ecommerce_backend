23f70a7799f492d10700f0fbb13bebf2a74b3c19
Zdeal <jedimastershake@gmail.com>
2021-07-24 22:52:05 -0400

    working index.js models

--- models/index.js
+++ models/index.js
@@ -11,11 +11,25 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
