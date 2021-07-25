d184ca4e928c6094d1db30e05e412f385801b10a
Zdeal <jedimastershake@gmail.com>
2021-07-24 22:46:40 -0400

    starting model index

--- models/index.js
+++ models/index.js
@@ -5,8 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
+
 
 // Products belongToMany Tags (through ProductTag)
 
